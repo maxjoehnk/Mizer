3d66d69080716664dcea143749d7d5bce59be7cc
Max Jöhnk <maxjoehnk@gmail.com>
2020-08-04 23:02:10 +0200

    fix(cli): add use statement

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use std::time::Duration;
 
+use structopt::StructOpt;
+
 #[cfg(feature = "export_metrics")]
 use metrics_runtime::{exporters::HttpExporter, observers::PrometheusBuilder, Receiver};
 

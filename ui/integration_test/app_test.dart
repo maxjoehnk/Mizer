9fcb7823e8632407537056575edf4a92585f4a52
Max Jöhnk <maxjoehnk@gmail.com>
2021-01-21 19:55:30 +0100

    fix(ui): tests reference old package name

--- ui/integration_test/app_test.dart
+++ ui/integration_test/app_test.dart
@@ -9,7 +9,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 
-import 'package:ui/main.dart' as app;
+import 'package:mizer/main.dart' as app;
 
 void main() => run(_testMain);
 

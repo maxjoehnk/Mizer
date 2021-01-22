36b3a198a7c75dbe7c5395c916ebe8ae4b821ea6
Max Jöhnk <maxjoehnk@gmail.com>
2021-01-22 18:15:53 +0100

    refactor(api): move proto buf files to api folder

--- api/grpc/build.rs
+++ api/grpc/build.rs
@@ -1,10 +1,10 @@
 fn main() {
     protoc_rust_grpc::Codegen::new()
         .out_dir("src/protos")
-        .input("../../proto/protos/fixtures.proto")
-        .input("../../proto/protos/nodes.proto")
-        .input("../../proto/protos/session.proto")
-        .include("../../proto/protos")
+        .input("../proto/protos/fixtures.proto")
+        .input("../proto/protos/nodes.proto")
+        .input("../proto/protos/session.proto")
+        .include("../proto/protos")
         .run()
         .expect("protoc-rust-grpc");
 }

616c5751d69dc5dc8a0ff388ba31fc79a6fdb888
Max Jöhnk <maxjoehnk@gmail.com>
2021-03-16 22:23:17 +0100

    feat(ui): allow direct connect to session

--- ui/lib/session/title_screen.dart
+++ ui/lib/session/title_screen.dart
@@ -17,11 +17,80 @@ class TitleScreen extends StatelessWidget {
           Text("Mizer", style: textTheme.headline2),
           Row(children: [
             Expanded(child: SessionList(this.selectSession)),
-            Expanded(child: CreateSession()),
+            Expanded(
+                child: Column(
+              children: [
+                JoinSession(this.selectSession),
+                CreateSession(),
+              ],
+            )),
           ]),
         ],
       ),
     );
+  }
+}
+
+class JoinSession extends StatelessWidget {
+  final Function(AvailableSession) selectSession;
+
+  JoinSession(this.selectSession);
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Row(children: [
+            Padding(
+              padding: const EdgeInsets.only(right: 16.0),
+              child: Icon(Icons.link),
+            ),
+            Text("Join Session")
+          ]),
+        ),
+        onTap: () => showDialog(context: context, builder: (context) => JoinSessionDialog()).then((value) {
+          if (value == null) {
+            return;
+          }
+          this.selectSession(value);
+        }));
+  }
+}
+
+class JoinSessionDialog extends StatelessWidget {
+  final TextEditingController ipController = TextEditingController();
+  final TextEditingController portController = TextEditingController(text: "50051");
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+        title: Text("Join Session"),
+        actions: [
+          ElevatedButton(
+              child: Text("Connect"),
+              onPressed: () {
+                String ip = this.ipController.text;
+                int port = int.parse(this.portController.text);
+                AvailableSession session = AvailableSession(host: ip, port: port);
+
+                Navigator.of(context).pop(session);
+              })
+        ],
+        content: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            TextField(
+                autofocus: true,
+                decoration: InputDecoration(labelText: "IP"),
+                controller: this.ipController),
+            TextField(
+              keyboardType: TextInputType.number,
+                autofocus: true,
+                decoration: InputDecoration(labelText: "Port"),
+                controller: this.portController),
+          ],
+        ));
   }
 }
 

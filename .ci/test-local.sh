f251707c7c9b29c995303ca6753741502e50aaf8
Max Jöhnk <maxjoehnk@gmail.com>
2021-06-28 07:59:14 +0200

    don't include rust in base docker image

--- .ci/test-local.sh
+++ .ci/test-local.sh
@@ -2,5 +2,5 @@
 set -e
 WORK_DIR=$PWD
 cd "$(dirname "$0")"
-docker build -t mizer-build:latest .
+docker build -t mizer-build:latest --target local .
 docker run -it -v $WORK_DIR:/work -w /work mizer-build make build

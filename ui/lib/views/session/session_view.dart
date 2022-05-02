73962fca2c2f51f4c3e3261ff2f6eea87caff407
Max Jöhnk <maxjoehnk@gmail.com>
2022-05-02 20:49:35 +0200

    feat(session): show session members as table

--- ui/lib/views/session/session_view.dart
+++ ui/lib/views/session/session_view.dart
@@ -2,33 +2,24 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:mizer/state/session_bloc.dart';
 import 'package:mizer/protos/session.pb.dart';
+import 'package:mizer/widgets/panel.dart';
+import 'package:mizer/widgets/table/table.dart';
 
 class SessionView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BlocBuilder<SessionBloc, Session>(
-        builder: (context, session) => ListView(
-            padding: const EdgeInsets.all(8.0),
-            children: session.devices.map((device) => SessionCard(device)).toList()));
-  }
-}
-
-class SessionCard extends StatelessWidget {
-  final SessionDevice device;
-
-  SessionCard(this.device);
-
-  @override
-  Widget build(BuildContext context) {
-    return Card(
-        child: Padding(
-      padding: const EdgeInsets.all(16.0),
-      child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-        Text(device.name),
-        Text(device.ips.join(", ")),
-        Text("Clock: ${device.clock.master ? "master" : "slave"} (+${device.clock.drift}ms)"),
-        Text("Ping: ${device.ping}ms")
-      ]),
-    ));
+        builder: (context, session) => Panel(
+          label: "Session",
+          child: MizerTable(
+              columns: [Text("Name"), Text("IP"), Text("Role"), Text("Clock"), Text("Ping")],
+              rows: session.devices.map((device) => MizerTableRow(cells: [
+                Text(device.name),
+                Text(device.ips.join(", ")),
+                Text(device.clock.master ? "master" : "slave"),
+                Text("+${device.clock.drift}ms"),
+                Text("${device.ping}ms")
+              ])).toList(),
+        )));
   }
 }

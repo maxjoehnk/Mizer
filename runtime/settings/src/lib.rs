ae3c658466ead195c2819e993041c8cd605c7a50
Max Jöhnk <maxjoehnk@gmail.com>
2022-05-30 19:56:40 +0200

    fix(settings): unable to store preferences when settings dir does not exist

--- runtime/settings/src/lib.rs
+++ runtime/settings/src/lib.rs
@@ -53,6 +53,7 @@ impl Settings {
     }
 
     pub fn save_to<P: AsRef<Path>>(&self, path: P) -> anyhow::Result<()> {
+        std::fs::create_dir_all(path.as_ref().parent().ok_or_else(|| anyhow::anyhow!("Invalid config path"))?)?;
         let mut file = std::fs::File::create(path)?;
         let file_contents = toml::to_vec(self)?;
         file.write_all(&file_contents)?;

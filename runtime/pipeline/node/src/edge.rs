1eb83246321d1e7f0d460f3e0a2854232a88aafb
Max Jöhnk <maxjoehnk@gmail.com>
2022-09-08 07:37:20 +0200

    implement fall and both edge modes

--- runtime/pipeline/node/src/edge.rs
+++ runtime/pipeline/node/src/edge.rs
@@ -2,9 +2,10 @@
 pub struct Edge {
     mode: EdgeMode,
     last_value: Option<f64>,
+    last_trigger: bool,
 }
 
-#[derive(Debug, Default)]
+#[derive(Debug, Default, PartialEq)]
 pub enum EdgeMode {
     #[default]
     Rise,
@@ -17,88 +18,271 @@ impl Edge {
         Default::default()
     }
 
+    // This is only used in test
+    #[allow(dead_code)]
+    pub(crate) fn with_mode(mode: EdgeMode) -> Self {
+        Self {
+            mode,
+            ..Self::default()
+        }
+    }
+
     pub fn update(&mut self, value: f64) -> Option<bool> {
+        if self.mode == EdgeMode::Both {
+            let mut result = true;
+
+            if let Some(last_value) = self.last_value {
+                let did_keep_value =
+                    both_above_zero(last_value, value) || both_are_zero(last_value, value);
+                if did_keep_value && self.last_trigger {
+                    result = false;
+                }
+            }
+
+            self.last_value = Some(value);
+            self.last_trigger = result;
+
+            return Some(result);
+        }
+
         if let Some(last_value) = self.last_value {
-            if last_value > 0f64 && value > 0f64 {
+            if both_above_zero(last_value, value) {
                 return None;
             }
-            if (last_value - value) <= f64::EPSILON && value <= f64::EPSILON {
+            if both_are_zero(last_value, value) {
                 return None;
             }
         }
         self.last_value = Some(value);
-        Some(value > 0f64)
+
+        match self.mode {
+            EdgeMode::Fall => Some(value <= 0f64),
+            EdgeMode::Rise => Some(value > 0f64),
+            EdgeMode::Both => unreachable!(),
+        }
     }
+}
+
+fn both_above_zero(last_value: f64, value: f64) -> bool {
+    last_value > 0f64 && value > 0f64
+}
+
+fn both_are_zero(last_value: f64, value: f64) -> bool {
+    (last_value - value) <= f64::EPSILON && value <= f64::EPSILON
 }
 
 #[cfg(test)]
 mod tests {
-    use test_case::test_case;
+    mod mode_rise {
+        use test_case::test_case;
 
-    use crate::edge::Edge;
+        use crate::edge::Edge;
 
-    #[test]
-    fn update_with_zero_should_return_false() {
-        let mut edge = Edge::new();
+        #[test]
+        fn update_with_zero_should_return_false() {
+            let mut edge = Edge::new();
 
-        let result = edge.update(0f64);
+            let result = edge.update(0f64);
 
-        assert_eq!(Some(false), result);
+            assert_eq!(Some(false), result);
+        }
+
+        #[test]
+        fn update_with_value_above_zero_should_return_true() {
+            let mut edge = Edge::new();
+
+            let result = edge.update(1f64);
+
+            assert_eq!(Some(true), result);
+        }
+
+        #[test_case(1f64)]
+        #[test_case(0f64)]
+        fn same_value_twice_in_a_row_should_return_none(value: f64) {
+            let mut edge = Edge::new();
+            edge.update(value);
+
+            let result = edge.update(value);
+
+            assert_eq!(None, result);
+        }
+
+        #[test_case(0.5f64, 1f64)]
+        #[test_case(1f64, 0.5f64)]
+        fn returning_two_values_above_zero_after_each_other_should_return_none(
+            first: f64,
+            second: f64,
+        ) {
+            let mut edge = Edge::new();
+            edge.update(first);
+
+            let result = edge.update(second);
+
+            assert_eq!(None, result);
+        }
+
+        #[test_case(0.5f64)]
+        #[test_case(1f64)]
+        fn rising_edge_should_return_true(value: f64) {
+            let mut edge = Edge::new();
+            edge.update(0f64);
+
+            let result = edge.update(value);
+
+            assert_eq!(Some(true), result);
+        }
+
+        #[test_case(0.5f64)]
+        #[test_case(1f64)]
+        fn falling_edge_should_return_false(value: f64) {
+            let mut edge = Edge::new();
+            edge.update(value);
+
+            let result = edge.update(0f64);
+
+            assert_eq!(Some(false), result);
+        }
     }
 
-    #[test]
-    fn update_with_value_above_zero_should_return_true() {
-        let mut edge = Edge::new();
+    mod mode_fall {
+        use test_case::test_case;
 
-        let result = edge.update(1f64);
+        use crate::edge::{Edge, EdgeMode};
 
-        assert_eq!(Some(true), result);
+        #[test]
+        fn update_with_zero_should_return_true() {
+            let mut edge = Edge::with_mode(EdgeMode::Fall);
+
+            let result = edge.update(0f64);
+
+            assert_eq!(Some(true), result);
+        }
+
+        #[test]
+        fn update_with_value_above_zero_should_return_false() {
+            let mut edge = Edge::with_mode(EdgeMode::Fall);
+
+            let result = edge.update(1f64);
+
+            assert_eq!(Some(false), result);
+        }
+
+        #[test_case(1f64)]
+        #[test_case(0f64)]
+        fn same_value_twice_in_a_row_should_return_none(value: f64) {
+            let mut edge = Edge::with_mode(EdgeMode::Fall);
+            edge.update(value);
+
+            let result = edge.update(value);
+
+            assert_eq!(None, result);
+        }
+
+        #[test_case(0.5f64, 1f64)]
+        #[test_case(1f64, 0.5f64)]
+        fn returning_two_values_above_zero_after_each_other_should_return_none(
+            first: f64,
+            second: f64,
+        ) {
+            let mut edge = Edge::with_mode(EdgeMode::Fall);
+            edge.update(first);
+
+            let result = edge.update(second);
+
+            assert_eq!(None, result);
+        }
+
+        #[test_case(0.5f64)]
+        #[test_case(1f64)]
+        fn rising_edge_should_return_false(value: f64) {
+            let mut edge = Edge::with_mode(EdgeMode::Fall);
+            edge.update(0f64);
+
+            let result = edge.update(value);
+
+            assert_eq!(Some(false), result);
+        }
+
+        #[test_case(0.5f64)]
+        #[test_case(1f64)]
+        fn falling_edge_should_return_true(value: f64) {
+            let mut edge = Edge::with_mode(EdgeMode::Fall);
+            edge.update(value);
+
+            let result = edge.update(0f64);
+
+            assert_eq!(Some(true), result);
+        }
     }
 
-    #[test_case(1f64)]
-    #[test_case(0f64)]
-    fn same_value_twice_in_a_row_should_return_none(value: f64) {
-        let mut edge = Edge::new();
-        edge.update(value);
+    mod mode_both {
+        use test_case::test_case;
 
-        let result = edge.update(value);
+        use crate::edge::{Edge, EdgeMode};
 
-        assert_eq!(None, result);
-    }
+        #[test]
+        fn update_with_zero_should_return_true() {
+            let mut edge = Edge::with_mode(EdgeMode::Both);
 
-    #[test_case(0.5f64, 1f64)]
-    #[test_case(1f64, 0.5f64)]
-    fn returning_two_values_above_zero_after_each_other_should_return_none(
-        first: f64,
-        second: f64,
-    ) {
-        let mut edge = Edge::new();
-        edge.update(first);
+            let result = edge.update(0f64);
 
-        let result = edge.update(second);
+            assert_eq!(Some(true), result);
+        }
 
-        assert_eq!(None, result);
-    }
+        #[test]
+        fn update_with_value_above_zero_should_return_true() {
+            let mut edge = Edge::with_mode(EdgeMode::Both);
 
-    #[test_case(0.5f64)]
-    #[test_case(1f64)]
-    fn rising_edge_should_return_true(value: f64) {
-        let mut edge = Edge::new();
-        edge.update(0f64);
+            let result = edge.update(1f64);
 
-        let result = edge.update(value);
+            assert_eq!(Some(true), result);
+        }
 
-        assert_eq!(Some(true), result);
-    }
+        #[test_case(1f64)]
+        #[test_case(0f64)]
+        fn same_value_twice_in_a_row_should_return_false(value: f64) {
+            let mut edge = Edge::with_mode(EdgeMode::Both);
+            edge.update(value);
 
-    #[test_case(0.5f64)]
-    #[test_case(1f64)]
-    fn falling_edge_should_return_false(value: f64) {
-        let mut edge = Edge::new();
-        edge.update(value);
+            let result = edge.update(value);
 
-        let result = edge.update(0f64);
+            assert_eq!(Some(false), result);
+        }
 
-        assert_eq!(Some(false), result);
+        #[test_case(0.5f64, 1f64)]
+        #[test_case(1f64, 0.5f64)]
+        fn returning_two_values_above_zero_after_each_other_should_return_none(
+            first: f64,
+            second: f64,
+        ) {
+            let mut edge = Edge::with_mode(EdgeMode::Fall);
+            edge.update(first);
+
+            let result = edge.update(second);
+
+            assert_eq!(None, result);
+        }
+
+        #[test_case(0.5f64)]
+        #[test_case(1f64)]
+        fn rising_edge_should_return_false(value: f64) {
+            let mut edge = Edge::with_mode(EdgeMode::Fall);
+            edge.update(0f64);
+
+            let result = edge.update(value);
+
+            assert_eq!(Some(false), result);
+        }
+
+        #[test_case(0.5f64)]
+        #[test_case(1f64)]
+        fn falling_edge_should_return_true(value: f64) {
+            let mut edge = Edge::with_mode(EdgeMode::Fall);
+            edge.update(value);
+
+            let result = edge.update(0f64);
+
+            assert_eq!(Some(true), result);
+        }
     }
 }

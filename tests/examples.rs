ac57d378c1fb08dd9497cc220d33a5c81332560b
Max Jöhnk <maxjoehnk@gmail.com>
2022-11-10 21:56:13 +0100

    tests(examples): add tests for all example projects

--- tests/examples.rs
+++ tests/examples.rs
@@ -4,15 +4,23 @@ use mizer::{build_runtime, Flags};
 use test_case::test_case;
 
 #[test_case("artnet"; "artnet")]
+#[test_case("effects"; "effects")]
 #[test_case("fixture"; "fixture")]
+#[test_case("history"; "history")]
+#[test_case("inputs"; "inputs")]
 #[test_case("laser"; "laser")]
+#[test_case("media"; "media")]
+#[test_case("midi"; "midi")]
+#[test_case("mqtt"; "mqtt")]
+#[test_case("operations"; "operations")]
+#[test_case("osc"; "osc")]
 #[test_case("pixels"; "pixels")]
 #[test_case("plan"; "plan")]
+#[test_case("presets"; "presets")]
 #[test_case("sacn"; "sacn")]
 #[test_case("sequence"; "sequence")]
+#[test_case("sequencer"; "sequencer")]
 #[test_case("video"; "video")]
-#[test_case("inputs"; "inputs")]
-#[test_case("osc"; "osc")]
 #[tokio::test]
 async fn test_build_project_pipeline(project: &str) {
     let handle = tokio::runtime::Handle::current();

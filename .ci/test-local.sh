a57f26e410040b353973ee193d13022ab8b5f344
Max Jöhnk <maxjoehnk@gmail.com>
2023-05-03 23:34:52 +0200

    fix minor linting issues in ci files

--- .ci/test-local.sh
+++ .ci/test-local.sh
@@ -3,4 +3,4 @@ set -e
 WORK_DIR=$PWD
 cd "$(dirname "$0")"
 docker build -t mizer-build:latest .
-docker run --rm -it -v $WORK_DIR:/work -w /work mizer-build make mizer.zip
+docker run --rm -it -v "$WORK_DIR":/work -w /work mizer-build make mizer.zip

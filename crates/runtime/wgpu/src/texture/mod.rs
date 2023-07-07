6094ea4dd31043a2b9613c300f43420dfcdc9d96
Max Jöhnk <maxjoehnk@gmail.com>
2023-07-07 18:05:19 +0200

    auto resize texture in texture source stage

--- crates/runtime/wgpu/src/texture/mod.rs
+++ crates/runtime/wgpu/src/texture/mod.rs
@@ -8,6 +8,8 @@ mod static_texture;
 mod texture_provider;
 mod texture_registry;
 
+const TEXTURE_FORMAT: wgpu::TextureFormat = wgpu::TextureFormat::Rgba8UnormSrgb;
+
 pub struct Texture {
     pub texture: wgpu::Texture,
     pub view: wgpu::TextureView,
@@ -21,7 +23,7 @@ impl Texture {
         let texture = context.create_texture(
             provider.width(),
             provider.height(),
-            wgpu::TextureFormat::Rgba8UnormSrgb,
+            TEXTURE_FORMAT,
             wgpu::TextureUsages::TEXTURE_BINDING | wgpu::TextureUsages::COPY_DST,
             None,
         );
@@ -69,7 +71,7 @@ impl Texture {
                 label: None,
             });
 
-        let texture = Self {
+        let mut texture = Self {
             texture,
             view,
             sampler,
@@ -84,16 +86,56 @@ impl Texture {
     }
 
     pub fn render(
-        &self,
+        &mut self,
         context: &WgpuContext,
         provider: &mut impl TextureProvider,
     ) -> anyhow::Result<()> {
         let width = provider.width();
         let height = provider.height();
+        if self.texture.width() != width || self.texture.height() != height {
+            log::debug!(
+                "Texture size changed from {}x{} to {}x{}",
+                self.texture.width(),
+                self.texture.height(),
+                width,
+                height
+            );
+            self.recreate_texture(context, width, height);
+        }
         if let Some(data) = provider.data()? {
             context.write_texture(&self.texture, data.as_ref(), width, height);
         }
 
         Ok(())
+    }
+
+    fn recreate_texture(&mut self, context: &WgpuContext, width: u32, height: u32) {
+        self.texture.destroy();
+        self.texture = context.create_texture(
+            width,
+            height,
+            TEXTURE_FORMAT,
+            wgpu::TextureUsages::TEXTURE_BINDING | wgpu::TextureUsages::COPY_DST,
+            None,
+        );
+        self.view = self
+            .texture
+            .create_view(&wgpu::TextureViewDescriptor::default());
+        self.bind_group = context
+            .device
+            .create_bind_group(&wgpu::BindGroupDescriptor {
+                layout: &self.bind_group_layout,
+                entries: &[
+                    wgpu::BindGroupEntry {
+                        binding: 0,
+                        resource: wgpu::BindingResource::TextureView(&self.view),
+                    },
+                    wgpu::BindGroupEntry {
+                        binding: 1,
+                        resource: wgpu::BindingResource::Sampler(&self.sampler),
+                    },
+                ],
+                label: None,
+            });
     }
 }

968ae48865690e0493bd3e8818b10d1a2dba4c5e
Max Jöhnk <maxjoehnk@gmail.com>
2023-10-27 07:35:09 +0200

    feat(nodes/preset): add outputs to color, intensity and shutter preset nodes

--- crates/runtime/pipeline/nodes/fixture/src/preset.rs
+++ crates/runtime/pipeline/nodes/fixture/src/preset.rs
@@ -5,6 +5,10 @@ use mizer_fixtures::programmer::{Preset, PresetId};
 use mizer_node::edge::Edge;
 use mizer_node::*;
 
+const CALL_PORT: &str = "Call";
+const COLOR_OUTPUT: &str = "Color";
+const VALUE_OUTPUT: &str = "Value";
+
 #[derive(Debug, Clone, Deserialize, Serialize, PartialEq, Eq)]
 pub struct PresetNode {
     pub id: PresetId,
@@ -60,7 +64,17 @@ impl PipelineNode for PresetNode {
     }
 
     fn list_ports(&self) -> Vec<(PortId, PortMetadata)> {
-        vec![input_port!("Call", PortType::Single)]
+        let mut ports = vec![input_port!(CALL_PORT, PortType::Single)];
+
+        match self.id {
+            PresetId::Color(_) => ports.push(output_port!(COLOR_OUTPUT, PortType::Color)),
+            PresetId::Intensity(_) | PresetId::Shutter(_) => {
+                ports.push(output_port!(VALUE_OUTPUT, PortType::Single))
+            }
+            PresetId::Position(_) => {}
+        }
+
+        ports
     }
 
     fn node_type(&self) -> NodeType {
@@ -78,6 +92,27 @@ impl ProcessingNode for PresetNode {
                 if let Some(true) = state.update(value) {
                     programmer.call_preset(&fixture_manager.presets, self.id);
                 }
+            }
+
+            match self.id {
+                PresetId::Color(id) => {
+                    if let Some(preset) = fixture_manager.presets.color.get(&id) {
+                        let (red, green, blue) = preset.value;
+                        let color = Color::rgb(red, green, blue);
+                        context.write_port(COLOR_OUTPUT, color);
+                    }
+                }
+                PresetId::Intensity(id) => {
+                    if let Some(preset) = fixture_manager.presets.intensity.get(&id) {
+                        context.write_port(VALUE_OUTPUT, preset.value);
+                    }
+                }
+                PresetId::Shutter(id) => {
+                    if let Some(preset) = fixture_manager.presets.shutter.get(&id) {
+                        context.write_port(VALUE_OUTPUT, preset.value);
+                    }
+                }
+                _ => {}
             }
         }
 

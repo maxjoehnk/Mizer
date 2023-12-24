b45da35b11db9e8387f11cd32192e30babc45d38
Max Jöhnk <maxjoehnk@gmail.com>
2023-12-24 10:35:20 +0100

    fix(patch): change address dialog has wrong label

--- crates/ui/lib/views/patch/fixture_table.dart
+++ crates/ui/lib/views/patch/fixture_table.dart
@@ -55,14 +55,27 @@ class FixtureTable extends StatelessWidget {
     var row = MizerTableRow(
       cells: [
         Text(fixture.id.toString()),
-        nameField(context, fixture, (name) => onUpdateFixture(fixture.id, UpdateFixtureRequest(name: name))),
-        addressField(context, fixture, (address) => onUpdateFixture(fixture.id, UpdateFixtureRequest(address: address))),
+        nameField(context, fixture,
+            (name) => onUpdateFixture(fixture.id, UpdateFixtureRequest(name: name))),
+        addressField(context, fixture,
+            (address) => onUpdateFixture(fixture.id, UpdateFixtureRequest(address: address))),
         Text(fixture.manufacturer),
         Text(fixture.model),
         Text(fixture.mode),
-        invertedAxisField(context, "Invert Pan", fixture.config.invertPan, (invertPan) => onUpdateFixture(fixture.id, UpdateFixtureRequest(invertPan: invertPan))),
-        invertedAxisField(context, "Invert Tilt", fixture.config.invertTilt, (invertTilt) => onUpdateFixture(fixture.id, UpdateFixtureRequest(invertTilt: invertTilt))),
-        reversePixelField(context, "Reverse Pixel Order", fixture.config.reversePixelOrder, (reversePixelOrder) => onUpdateFixture(fixture.id, UpdateFixtureRequest(reversePixelOrder: reversePixelOrder))),
+        invertedAxisField(context, "Invert Pan", fixture.config.invertPan,
+            (invertPan) => onUpdateFixture(fixture.id, UpdateFixtureRequest(invertPan: invertPan))),
+        invertedAxisField(
+            context,
+            "Invert Tilt",
+            fixture.config.invertTilt,
+            (invertTilt) =>
+                onUpdateFixture(fixture.id, UpdateFixtureRequest(invertTilt: invertTilt))),
+        reversePixelField(
+            context,
+            "Reverse Pixel Order",
+            fixture.config.reversePixelOrder,
+            (reversePixelOrder) => onUpdateFixture(
+                fixture.id, UpdateFixtureRequest(reversePixelOrder: reversePixelOrder))),
       ],
       onTap: () => onSelect(fixture.id, !selected),
       onDoubleTap: () => onSelectSimilar(fixture),
@@ -73,49 +86,63 @@ class FixtureTable extends StatelessWidget {
 }
 
 Widget nameField(BuildContext context, Fixture fixture, Function(String) onUpdate) {
-  return PopupTableCell(child: Text(fixture.name), popup: PopupInput(
-    title: "Name",
-    value: fixture.name,
-    onChange: onUpdate,
-  ));
+  return PopupTableCell(
+      child: Text(fixture.name),
+      popup: PopupInput(
+        title: "Name",
+        value: fixture.name,
+        onChange: onUpdate,
+      ));
 }
 
 Widget addressField(BuildContext context, Fixture fixture, Function(FixtureAddress) onUpdate) {
-  return PopupTableCell(child: Text("${fixture.universe}.${fixture.channel.toString().padLeft(3, "0")}"), popup: PopupDmxAddressInput(
-    title: "Name",
-    value: FixtureAddress(universe: fixture.universe, channel: fixture.channel),
-    onChange: onUpdate,
-  ));
+  return PopupTableCell(
+      child: Text("${fixture.universe}.${fixture.channel.toString().padLeft(3, "0")}"),
+      popup: PopupDmxAddressInput(
+        title: "Address",
+        value: FixtureAddress(universe: fixture.universe, channel: fixture.channel),
+        onChange: onUpdate,
+      ));
 }
 
-Widget invertedAxisField(BuildContext context, String title, bool inverted, Function(bool) onUpdate) {
+Widget invertedAxisField(
+    BuildContext context, String title, bool inverted, Function(bool) onUpdate) {
   return PopupTableCell(
       child: Text(inverted ? "Inverted" : "Normal",
           style: inverted ? null : TextStyle(color: Colors.white54)),
       popup: PopupSelect(title: title, items: [
-        SelectItem(title: "Normal", onTap: () {
-          onUpdate(false);
-          Navigator.of(context).pop();
-        }),
-        SelectItem(title: "Inverted", onTap: () {
-          onUpdate(true);
-          Navigator.of(context).pop();
-        })
+        SelectItem(
+            title: "Normal",
+            onTap: () {
+              onUpdate(false);
+              Navigator.of(context).pop();
+            }),
+        SelectItem(
+            title: "Inverted",
+            onTap: () {
+              onUpdate(true);
+              Navigator.of(context).pop();
+            })
       ]));
 }
 
-Widget reversePixelField(BuildContext context, String title, bool reversed, Function(bool) onUpdate) {
+Widget reversePixelField(
+    BuildContext context, String title, bool reversed, Function(bool) onUpdate) {
   return PopupTableCell(
       child: Text(reversed ? "Reversed" : "Normal",
           style: reversed ? null : TextStyle(color: Colors.white54)),
       popup: PopupSelect(title: title, items: [
-        SelectItem(title: "Normal", onTap: () {
-          onUpdate(false);
-          Navigator.of(context).pop();
-        }),
-        SelectItem(title: "Reversed", onTap: () {
-          onUpdate(true);
-          Navigator.of(context).pop();
-        })
+        SelectItem(
+            title: "Normal",
+            onTap: () {
+              onUpdate(false);
+              Navigator.of(context).pop();
+            }),
+        SelectItem(
+            title: "Reversed",
+            onTap: () {
+              onUpdate(true);
+              Navigator.of(context).pop();
+            })
       ]));
 }

5b76e408e8a60c8dca55712e4c7f96b949ef270d
Max Jöhnk <maxjoehnk@gmail.com>
2024-01-07 18:31:13 +0100

    fix(media): thumbnails are not properly generated for svgs

--- crates/components/media/src/media_handlers/svg_handler.rs
+++ crates/components/media/src/media_handlers/svg_handler.rs
@@ -2,9 +2,9 @@ use crate::documents::MediaType;
 use crate::file_storage::FileStorage;
 use crate::media_handlers::{MediaHandler, THUMBNAIL_SIZE};
 use resvg::tiny_skia::{self, Pixmap};
-use resvg::usvg::{self, NodeExt, Options, TreeParsing};
+use resvg::usvg::{self, Options, TreeParsing};
 use std::io::{Read, Write};
-use std::path::Path;
+use std::path::{Path, PathBuf};
 
 #[derive(Clone)]
 pub struct SvgHandler;
@@ -22,27 +22,57 @@ impl MediaHandler for SvgHandler {
         storage: &FileStorage,
         _content_type: &str,
     ) -> anyhow::Result<()> {
-        let mut file = std::fs::File::open(&path)?;
+        let tree = Self::read_svg(&path)?;
+        let buffer = Self::generate_thumbnail(tree)?;
+
+        let thumbnail_path = storage.get_thumbnail_path(&path);
+        Self::write_thumbnail(&buffer, thumbnail_path)?;
+        Ok(())
+    }
+}
+
+impl SvgHandler {
+    fn read_svg<P: AsRef<Path>>(path: &P) -> anyhow::Result<resvg::Tree> {
+        let mut file = std::fs::File::open(path)?;
         let mut buffer = Vec::new();
         file.read_to_end(&mut buffer)?;
         let tree = usvg::Tree::from_data(&buffer, &Options::default())?;
-        let mut pixmap = Pixmap::new(THUMBNAIL_SIZE, THUMBNAIL_SIZE).unwrap();
-        let mut pixel_buffer = pixmap.as_mut();
-        let bounding_box = tree.root.calculate_bbox().unwrap();
-        let scale = if bounding_box.width() > bounding_box.height() {
-            (THUMBNAIL_SIZE as f32) / bounding_box.width()
-        } else {
-            (THUMBNAIL_SIZE as f32) / bounding_box.height()
-        };
         let tree = resvg::Tree::from_usvg(&tree);
-        tree.render(
-            tiny_skia::Transform::from_scale(scale, scale),
-            &mut pixel_buffer,
+
+        Ok(tree)
+    }
+
+    fn get_thumbnail_transform(
+        tree: &resvg::Tree,
+    ) -> anyhow::Result<(tiny_skia::IntSize, tiny_skia::Transform)> {
+        let size = if tree.size.width() > tree.size.height() {
+            tree.size.to_int_size().scale_to_width(THUMBNAIL_SIZE)
+        } else {
+            tree.size.to_int_size().scale_to_height(THUMBNAIL_SIZE)
+        }
+        .ok_or_else(|| anyhow::anyhow!("Failed to calculate thumbnail size"))?;
+
+        let transform = tiny_skia::Transform::from_scale(
+            size.width() as f32 / tree.size.width(),
+            size.height() as f32 / tree.size.height(),
         );
-        let buffer = pixmap.encode_png()?;
-        let thumbnail_path = storage.get_thumbnail_path(&path);
+        Ok((size, transform))
+    }
+
+    fn write_thumbnail(buffer: &[u8], thumbnail_path: PathBuf) -> anyhow::Result<()> {
         let mut file = std::fs::File::create(thumbnail_path)?;
-        file.write_all(&buffer)?;
+        file.write_all(buffer)?;
+
         Ok(())
+    }
+
+    fn generate_thumbnail(tree: resvg::Tree) -> anyhow::Result<Vec<u8>> {
+        let (size, transform) = Self::get_thumbnail_transform(&tree)?;
+
+        let mut pixmap = Pixmap::new(size.width(), size.height()).unwrap();
+        tree.render(transform, &mut pixmap.as_mut());
+        let buffer = pixmap.encode_png()?;
+
+        Ok(buffer)
     }
 }

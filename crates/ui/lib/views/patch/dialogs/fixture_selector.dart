3bca4b1fd78c66e4c38de52f05c00c826878c981
Max Jöhnk <maxjoehnk@gmail.com>
2024-02-06 17:15:42 +0100

    feat(patch): improve usability of fixture selector
    * combined search for manufacturer and fixtures
    * better visibility of selected items
    * auto select first manufacturer/fixture when previous selection is not applicable anymore

--- crates/ui/lib/views/patch/dialogs/fixture_selector.dart
+++ crates/ui/lib/views/patch/dialogs/fixture_selector.dart
@@ -1,5 +1,6 @@
 import 'package:collection/collection.dart';
 import 'package:flutter/material.dart';
+import 'package:mizer/extensions/list_extensions.dart';
 import 'package:mizer/extensions/map_extensions.dart';
 import 'package:mizer/protos/fixtures.pb.dart';
 
@@ -16,17 +17,21 @@ class FixtureSelector extends StatefulWidget {
 }
 
 class _FixtureSelectorState extends State<FixtureSelector> {
-  _ManufacturerGroup? manufacturer;
+  String? _manufacturerName;
   FixtureDefinition? definition;
   FixtureMode? mode;
+  String? search;
 
   @override
   void initState() {
     super.initState();
     if (widget.definition != null) {
-      this.manufacturer = widget.definitions
+      this._manufacturerName = _allDefinitions
           .groupByManufacturer()
-          .firstWhere((group) => group.name == widget.definition!.manufacturer);
+          .firstWhereOrNull((group) => group.name == widget.definition!.manufacturer)
+          ?.name;
+    } else {
+      this._manufacturerName = _allDefinitions.groupByManufacturer().firstOrNull?.name;
     }
     this.definition = widget.definition;
     this.mode = widget.mode;
@@ -34,69 +39,118 @@ class _FixtureSelectorState extends State<FixtureSelector> {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
+    return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        _FixtureSelectorColumn(
-            label: "Manufacturer",
-            children: widget.definitions
-                .groupByManufacturer()
-                .sortedBy((element) => element.name)
-                .map(_manufacturerItem)
-                .toList()),
-        _FixtureSelectorColumn(
-            label: "Fixtures", children: _definitions.map(_definitionItem).toList()),
-        _FixtureSelectorColumn(
-            label: "Modes", children: definition?.modes.map(_modeItem).toList() ?? []),
-        _SelectedFixtureMode(definition, mode)
+        SizedBox(
+          child: TextField(
+            autofocus: true,
+            decoration: InputDecoration(
+              labelText: "Search",
+              hintText: "Search",
+              prefixIcon: Icon(Icons.search),
+            ),
+            onChanged: (value) {
+              setState(() {
+                search = value;
+              });
+              if (_manufacturer == null) {
+                _setManufacturer(_manufacturers.firstOrNull?.name);
+              }
+            },
+          ),
+        ),
+        SizedBox(height: 16),
+        Expanded(
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              _FixtureSelectorColumn(
+                  label: "Manufacturer", children: _manufacturers.map(_manufacturerItem).toList()),
+              _FixtureSelectorColumn(
+                  label: "Fixtures", children: _definitions.map(_definitionItem).toList()),
+              _FixtureSelectorColumn(
+                  label: "Modes", children: definition?.modes.map(_modeItem).toList() ?? []),
+              _SelectedFixtureMode(definition, mode)
+            ],
+          ),
+        ),
       ],
     );
   }
 
+  void _setManufacturer(String? manufacturer) {
+    setState(() {
+      _manufacturerName = manufacturer;
+      definition = _definitions.firstOrNull;
+    });
+  }
+
+  List<_ManufacturerGroup> get _manufacturers {
+    return _allDefinitions.groupByManufacturer().sortedBy((element) => element.name).toList();
+  }
+
+  _ManufacturerGroup? get _manufacturer {
+    return _manufacturers.firstWhereOrNull((m) => m.name == _manufacturerName);
+  }
+
+  List<FixtureDefinition> get _allDefinitions {
+    return widget.definitions.definitions.search([
+      (d) => d.name,
+      (d) => d.manufacturer,
+    ], search).toList();
+  }
+
   List<FixtureDefinition> get _definitions {
-    if (manufacturer == null) {
-      return widget.definitions.definitions;
-    }
-    return manufacturer!.definitions;
+    return _allDefinitions.where((d) => d.manufacturer == _manufacturerName).toList();
   }
 
   FixtureColumnEntry _manufacturerItem(_ManufacturerGroup manufacturer) {
     var text = manufacturer.name;
-    var child = ListTile(
-      title: Text(text),
-      selected: manufacturer.name == this.manufacturer?.name,
-      onTap: () => setState(() {
-        this.manufacturer = manufacturer;
-        mode = null;
-        definition = null;
-      }),
+    var selected = manufacturer.name == this._manufacturerName;
+    var child = Container(
+      color: selected ? Colors.black26 : null,
+      child: ListTile(
+        title: Text(text),
+        subtitle: Text("${manufacturer.definitions.length} fixtures"),
+        onTap: () => setState(() {
+          _setManufacturer(manufacturer.name);
+          mode = null;
+        }),
+      ),
     );
 
     return FixtureColumnEntry(child: child, text: text);
   }
 
   FixtureColumnEntry _definitionItem(FixtureDefinition definition) {
-    var child = ListTile(
-        onTap: () => setState(() {
-              this.definition = definition;
-              mode = null;
-            }),
-        selected: definition.id == this.definition?.id,
-        title: Text(definition.name),
-        subtitle: Text(definition.provider));
+    var selected = definition.id == this.definition?.id;
+    var child = Container(
+      color: selected ? Colors.black26 : null,
+      child: ListTile(
+          onTap: () => setState(() {
+                this.definition = definition;
+                mode = null;
+              }),
+          title: Text(definition.name),
+          subtitle: Text(definition.provider)),
+    );
 
     return FixtureColumnEntry(child: child, text: definition.name);
   }
 
   FixtureColumnEntry _modeItem(FixtureMode mode) {
     var text = mode.name;
-    var child = ListTile(
-      title: Text(text),
-      onTap: () {
-        setState(() => this.mode = mode);
-        widget.onSelect(this.definition!, mode);
-      },
-      selected: this.mode?.name == mode.name,
+    var selected = this.mode?.name == mode.name;
+    var child = Container(
+      color: selected ? Colors.black26 : null,
+      child: ListTile(
+        title: Text(text),
+        onTap: () {
+          setState(() => this.mode = mode);
+          widget.onSelect(this.definition!, mode);
+        },
+      ),
     );
 
     return FixtureColumnEntry(child: child, text: text);
@@ -110,24 +164,14 @@ class FixtureColumnEntry {
   FixtureColumnEntry({required this.child, required this.text});
 }
 
-class _FixtureSelectorColumn extends StatefulWidget {
+class _FixtureSelectorColumn extends StatelessWidget {
   final String label;
   final List<FixtureColumnEntry> children;
 
   _FixtureSelectorColumn({required this.label, required this.children});
 
-  @override
-  State<_FixtureSelectorColumn> createState() => _FixtureSelectorColumnState();
-}
-
-class _FixtureSelectorColumnState extends State<_FixtureSelectorColumn> {
-  String _query = "";
-
   List<Widget> get _children {
-    return widget.children
-        .where((e) => e.text.toLowerCase().contains(_query))
-        .map((e) => e.child)
-        .toList(growable: false);
+    return children.map((e) => e.child).toList(growable: false);
   }
 
   @override
@@ -138,14 +182,10 @@ class _FixtureSelectorColumnState extends State<_FixtureSelectorColumn> {
         width: 256,
         padding: const EdgeInsets.all(4),
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            Text(widget.label, style: textTheme.subtitle2),
-            TextField(
-              decoration: InputDecoration(labelText: "Search"),
-              autofocus: true,
-              onChanged: _updateSearch,
-            ),
+            Text(label, style: textTheme.titleMedium, textAlign: TextAlign.center),
+            SizedBox(height: 16),
             Expanded(
               child: ListView(children: _children),
             ),
@@ -153,10 +193,6 @@ class _FixtureSelectorColumnState extends State<_FixtureSelectorColumn> {
         ),
       ),
     );
-  }
-
-  void _updateSearch(String query) {
-    setState(() => _query = query.toLowerCase());
   }
 }
 
@@ -202,13 +238,9 @@ class _SelectedFixtureMode extends StatelessWidget {
   }
 }
 
-extension _FixtureDefinitionGrouping on FixtureDefinitions? {
+extension _FixtureDefinitionGrouping on List<FixtureDefinition> {
   List<_ManufacturerGroup> groupByManufacturer() {
-    if (this == null) {
-      return [];
-    }
-    var groupedDefinitions =
-        groupBy<FixtureDefinition, String>(this!.definitions, (d) => d.manufacturer);
+    var groupedDefinitions = groupBy<FixtureDefinition, String>(this, (d) => d.manufacturer);
 
     return groupedDefinitions
         .mapToList((manufacturer, definitions) => _ManufacturerGroup(manufacturer, definitions));

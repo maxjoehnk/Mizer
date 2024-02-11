193dab27e0126d111ab9df15d8d4261f01c9fc3e
Max Jöhnk <maxjoehnk@gmail.com>
2024-02-11 23:31:47 +0100

    fix window setup on macos

--- crates/ui/lib/windows/main_window.dart
+++ crates/ui/lib/windows/main_window.dart
@@ -6,6 +6,9 @@ import 'package:nativeshell/nativeshell.dart';
 import '../navigation.dart';
 import 'base_window_state.dart';
 
+const double WIDTH = 800;
+const double HEIGHT = 700;
+
 class MainWindowState extends WindowState {
   @override
   Widget build(BuildContext context) {
@@ -23,12 +26,14 @@ class MainWindowState extends WindowState {
   }
 
   @override
-  WindowSizingMode get windowSizingMode => WindowSizingMode.manual;
+  WindowSizingMode get windowSizingMode => WindowSizingMode.atLeastIntrinsicSize;
 
   @override
-  Future<void> initializeWindow(Size contentSize) async {
+  Future<void> initializeWindow(Size intrinsicSize) async {
     await window.setTitle("Mizer");
     await window.setMaximized(true);
+    Size contentSize = Size(intrinsicSize.width > WIDTH ? intrinsicSize.width : WIDTH,
+        intrinsicSize.height > HEIGHT ? intrinsicSize.height : HEIGHT);
     await window.setGeometry(Geometry(minContentSize: Size(800, 700), contentSize: contentSize));
     await window.show();
   }

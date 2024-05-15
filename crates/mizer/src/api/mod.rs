a1048dfcd64e45f5e47452b0c14257d6fb2d3793
Max Jöhnk <maxjoehnk@gmail.com>
2024-05-15 12:14:38 +0200

    remove unused remote api module

--- crates/mizer/src/api/mod.rs
+++ crates/mizer/src/api/mod.rs
@@ -1,9 +1,7 @@
 pub use self::api_impl::*;
-//pub use self::remote_api_impl::*;
 pub use self::commands::*;
 pub use self::handler::*;
 
 mod api_impl;
-//mod remote_api_impl;
 mod commands;
 mod handler;

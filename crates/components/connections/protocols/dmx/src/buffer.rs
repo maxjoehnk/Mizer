06266c10b3850118bd43f2eef9118269fb8fcc64
Max Jöhnk <maxjoehnk@gmail.com>
2024-08-13 11:35:15 +0200

    feat(dmx): cleanup universes when no longer required

--- crates/components/connections/protocols/dmx/src/buffer.rs
+++ crates/components/connections/protocols/dmx/src/buffer.rs
@@ -1,8 +1,9 @@
-use dashmap::DashMap;
+use dashmap::{DashMap, DashSet};
 
 #[derive(Default)]
 pub struct DmxBuffer {
-    pub buffers: DashMap<u16, [u8; 512]>,
+    buffers: DashMap<u16, [u8; 512]>,
+    touched: DashSet<u16>,
 }
 
 impl DmxBuffer {
@@ -11,6 +12,7 @@ impl DmxBuffer {
         tracing::trace!("writing {}:{} => {}", universe, channel, value);
         let mut buffer = self.buffers.entry(universe).or_insert_with(|| [0; 512]);
         buffer[channel as usize] = value;
+        self.touched.insert(universe);
     }
 
     pub fn write_bulk(&self, universe: u16, channel: u16, values: &[u8]) {
@@ -23,11 +25,25 @@ impl DmxBuffer {
         for (i, value) in values.iter().enumerate() {
             buffer[i + channel as usize - 1usize] = *value;
         }
+        self.touched.insert(universe);
     }
 
     pub fn iter(&self) -> impl Iterator<Item = (u16, [u8; 512])> + '_ {
         self.buffers
             .iter()
-            .map(|entry| (*entry.key(), entry.value().clone()))
+            .map(|entry| (*entry.key(), *entry.value()))
+    }
+    
+    pub fn universes(&self) -> impl Iterator<Item = u16> + '_ {
+        self.buffers.iter().map(|entry| *entry.key())
+    }
+
+    pub(crate) fn pristine(&self) {
+        self.touched.clear();
+    }
+    
+    pub(crate) fn cleanup(&self) {
+        profiling::scope!("DmxBuffer::cleanup");
+        self.buffers.retain(|key, _| self.touched.contains(key));
     }
 }

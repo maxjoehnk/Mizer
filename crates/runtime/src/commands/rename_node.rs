d0e1aec221c6776cd7717596b68629418567ae59
Max Jöhnk <maxjoehnk@gmail.com>
2025-02-13 01:42:24 +0100

    fix(layouts): renaming node doesn't update node reference of layout controls

--- crates/runtime/src/commands/rename_node.rs
+++ crates/runtime/src/commands/rename_node.rs
@@ -1,7 +1,8 @@
 use serde::{Deserialize, Serialize};
 
 use crate::Pipeline;
-use mizer_commander::{Command, RefMut};
+use mizer_commander::{Command, Ref, RefMut};
+use mizer_layouts::LayoutStorage;
 use mizer_node::NodePath;
 use mizer_nodes::{ContainerNode, Node};
 
@@ -12,7 +13,7 @@ pub struct RenameNodeCommand {
 }
 
 impl<'a> Command<'a> for RenameNodeCommand {
-    type Dependencies = RefMut<Pipeline>;
+    type Dependencies = (RefMut<Pipeline>, Ref<LayoutStorage>);
     type State = Vec<(NodePath, Node)>;
     type Result = ();
 
@@ -20,28 +21,44 @@ impl<'a> Command<'a> for RenameNodeCommand {
         format!("Renaming Node '{}' to '{}'", self.path, self.new_name)
     }
 
-    fn apply(&self, pipeline: &mut Pipeline) -> anyhow::Result<(Self::Result, Self::State)> {
+    fn apply(&self, (pipeline, layout_storage): (&mut Pipeline, &LayoutStorage)) -> anyhow::Result<(Self::Result, Self::State)> {
         pipeline.rename_node(&self.path, self.new_name.clone())?;
         let previous_containers = self.rename_node_in_containers(pipeline)?;
+        self.rename_node_in_layouts(layout_storage, &self.path, &self.new_name);
 
         Ok(((), previous_containers))
     }
 
     fn revert(
         &self,
-        pipeline: &mut Pipeline,
+        (pipeline, layout_storage): (&mut Pipeline, &LayoutStorage),
         container_commands: Self::State,
     ) -> anyhow::Result<()> {
         pipeline.rename_node(&self.new_name, self.path.clone())?;
         for (path, container) in container_commands {
             pipeline.update_node(&path, container)?;
         }
+        self.rename_node_in_layouts(layout_storage, &self.new_name, &self.path);
 
         Ok(())
     }
 }
 
 impl RenameNodeCommand {
+    fn rename_node_in_layouts(&self, layout_storage: &LayoutStorage, from: &NodePath, to: &NodePath) {
+        let mut layouts = layout_storage.read();
+        for layout in &mut layouts {
+            for control in &mut layout.controls {
+                if let mizer_layouts::ControlType::Node { path } = &mut control.control_type {
+                    if path == from {
+                        *path = to.clone();
+                    }
+                }
+            }
+        }
+        layout_storage.set(layouts);
+    }
+    
     fn rename_node_in_containers(
         &self,
         pipeline: &mut Pipeline,

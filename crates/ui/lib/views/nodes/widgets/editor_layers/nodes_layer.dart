e26af836993b850df8139b9b6ca1cb0149ee0a55
Max Jöhnk <maxjoehnk@gmail.com>
2026-01-18 16:44:41 +0100

    perf(nodes): don't render off screen nodes

--- crates/ui/lib/views/nodes/widgets/editor_layers/nodes_layer.dart
+++ crates/ui/lib/views/nodes/widgets/editor_layers/nodes_layer.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/widgets.dart';
 import 'package:mizer/state/nodes_bloc.dart';
+import 'package:mizer/views/nodes/consts.dart';
 import 'package:provider/provider.dart';
 import 'package:vector_math/vector_math_64.dart';
 
@@ -17,68 +18,94 @@ class _NodesTargetState extends State<NodesTarget> {
 
   @override
   Widget build(BuildContext context) {
-    return Consumer<NodeEditorModel>(builder: (context, model, _) {
-      return SizedBox.expand(
-          child: Stack(
-        clipBehavior: Clip.none,
-        children: model.nodes.where((node) => !node.node.designer.hidden).map((node) {
-          var offset = node.offset;
-          if (currentOffset != null) {
-            if (model.selectedNode != null && model.selectedNode!.node.path == node.node.path ||
-                model.otherSelectedNodes.any((element) => element.node.path == node.node.path)) {
-              offset += currentOffset!;
-            }
-          }
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        return Consumer<NodeEditorModel>(builder: (context, model, _) {
+          final viewport = calculateViewport(constraints.biggest, model.transformationController.value, inflate: 200);
 
-          return Transform(
-            transform: model.transformationController.value *
-                Matrix4.translation(Vector3(offset.dx, offset.dy, 0)),
-            child: GestureDetector(
-                onPanStart: (e) {
-                  setState(() {
-                      startOffset = e.localPosition;
-                      currentOffset = Offset.zero;
-                    });
-                  if (model.isSelected(node.node.path)) {
-                    return;
-                  }
-                  model.selectNode(node);
-                },
-                onPanUpdate: (e) {
-                  if (startOffset == null) {
-                    return;
-                  }
-                  setState(() {
-                    currentOffset = e.localPosition - startOffset!;
-                  });
-                  model.updateMovement();
-                },
-                onPanEnd: (e) {
-                  NodesBloc bloc = context.read();
-                  List<MoveNode> movements = [];
-                  if (model.selectedNode != null) {
-                    model.selectedNode!.offset += currentOffset!;
-                    movements.add(MoveNode(model.selectedNode!.node.path, model.selectedNode!.getPosition()));
-                  }
-                  for (var selectedNode in model.otherSelectedNodes) {
-                    if (selectedNode.node.path == model.selectedNode?.node.path) {
-                      continue;
-                    }
-                    var node = model.nodes.firstWhere((element) => element.node.path == selectedNode.node.path);
-                    node.offset += currentOffset!;
-                    movements.add(MoveNode(selectedNode.node.path, node.getPosition()));
-                  }
-                  bloc.add(MoveNodes(movements));
-                  model.update();
-                  setState(() {
-                    startOffset = null;
-                    currentOffset = null;
-                  });
-                },
-                child: NodeControl(node)),
-          );
-        }).toList(),
-      ));
-    });
+          return SizedBox.expand(
+              child: Stack(
+            clipBehavior: Clip.none,
+            children: model.nodes.where((node) => !node.node.designer.hidden).where((node) {
+              final nodeRect = Rect.fromLTWH(node.offset.dx, node.offset.dy, NODE_BASE_WIDTH, 200);
+
+              return viewport.overlaps(nodeRect);
+            }).map((node) {
+              var offset = node.offset;
+              if (currentOffset != null) {
+                if (model.selectedNode != null && model.selectedNode!.node.path == node.node.path ||
+                    model.otherSelectedNodes.any((element) => element.node.path == node.node.path)) {
+                  offset += currentOffset!;
+                }
+              }
+
+              return Transform(
+                transform: model.transformationController.value *
+                    Matrix4.translation(Vector3(offset.dx, offset.dy, 0)),
+                child: GestureDetector(
+                    onPanStart: (e) {
+                      setState(() {
+                        startOffset = e.localPosition;
+                        currentOffset = Offset.zero;
+                      });
+                      if (model.isSelected(node.node.path)) {
+                        return;
+                      }
+                      model.selectNode(node);
+                    },
+                    onPanUpdate: (e) {
+                      if (startOffset == null) {
+                        return;
+                      }
+                      setState(() {
+                        currentOffset = e.localPosition - startOffset!;
+                      });
+                      model.updateMovement();
+                    },
+                    onPanEnd: (e) {
+                      NodesBloc bloc = context.read();
+                      List<MoveNode> movements = [];
+                      if (model.selectedNode != null) {
+                        model.selectedNode!.offset += currentOffset!;
+                        movements.add(
+                            MoveNode(model.selectedNode!.node.path, model.selectedNode!.getPosition()));
+                      }
+                      for (var selectedNode in model.otherSelectedNodes) {
+                        if (selectedNode.node.path == model.selectedNode?.node.path) {
+                          continue;
+                        }
+                        var node = model.nodes
+                            .firstWhere((element) => element.node.path == selectedNode.node.path);
+                        node.offset += currentOffset!;
+                        movements.add(MoveNode(selectedNode.node.path, node.getPosition()));
+                      }
+                      bloc.add(MoveNodes(movements));
+                      model.update();
+                      setState(() {
+                        startOffset = null;
+                        currentOffset = null;
+                      });
+                    },
+                    child: NodeControl(node)),
+              );
+            }).toList(),
+          ));
+        });
+      }
+    );
   }
+}
+
+Rect calculateViewport(Size size, Matrix4 transformMatrix, { double? inflate }) {
+  final inverse = Matrix4.inverted(transformMatrix);
+
+  final topLeft = MatrixUtils.transformPoint(inverse, Offset.zero);
+  final bottomRight = MatrixUtils.transformPoint(inverse, Offset(size.width, size.height));
+  final viewportRect = Rect.fromPoints(topLeft, bottomRight);
+
+  if (inflate == null) {
+    return viewportRect;
+  }
+
+  return viewportRect.inflate(inflate);
 }

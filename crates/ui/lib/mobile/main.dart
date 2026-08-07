16e38be838e89fb50e8fd767be63dc79cb751bca
Max Jöhnk <maxjoehnk@gmail.com>
2026-08-07 16:31:42 +0200

    fix(mobile): application doesn't start
    * add missing i18n configuration for mobile entrypoint
    * update gradle and bump dependencies

--- crates/ui/lib/mobile/main.dart
+++ crates/ui/lib/mobile/main.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_material_design_icons/flutter_material_design_icons.dart';
 import 'package:mizer/api/mobile/provider.dart';
 import 'package:mizer/app.dart';
 import 'package:mizer/i18n.dart';
-
 import 'package:mizer/mobile/fixture_list.dart';
 import 'package:mizer/mobile/navigation.dart';
 import 'package:mizer/mobile/sequence_list.dart';
@@ -18,30 +18,39 @@ void main() async {
 class MizerMobileUi extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MizerApp(
-      child: SessionSelector(
-          builder: (context, connection) => MobileApiProvider(
-              child: Scaffold(
-                appBar: AppBar(
-                  title: Text('Mizer'),
-                  actions: [
-                    PopupMenuButton(
-                      itemBuilder: (context) => [
-                        PopupMenuItem(
-                          child: Text('Disconnect'),
-                          onTap: () => connection.disconnect(),
-                        )
-                      ],
-                    )
-                  ],
+    return I18n(
+      initialLocale: const Locale('en'),
+      supportedLocales: ['en'.asLocale],
+      localizationsDelegates: [
+        GlobalMaterialLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+      ],
+      child: MizerApp(
+        child: SessionSelector(
+            builder: (context, connection) => MobileApiProvider(
+                child: Scaffold(
+                  appBar: AppBar(
+                    title: Text('Mizer'),
+                    actions: [
+                      PopupMenuButton(
+                        itemBuilder: (context) => [
+                          PopupMenuItem(
+                            child: Text('Disconnect'),
+                            onTap: () => connection.disconnect(),
+                          )
+                        ],
+                      )
+                    ],
+                  ),
+                  body: Navigation(tabs: [
+                    MobileTab(child: FixtureList(), title: "Patch".i18n, icon: MdiIcons.spotlight),
+                    MobileTab(child: SequenceList(), title: "Sequences".i18n, icon: MdiIcons.animationPlayOutline),
+                  ]),
                 ),
-                body: Navigation(tabs: [
-                  MobileTab(child: FixtureList(), title: "Patch".i18n, icon: MdiIcons.spotlight),
-                  MobileTab(child: SequenceList(), title: "Sequences".i18n, icon: MdiIcons.animationPlayOutline),
-                ]),
-              ),
-              host: connection.session.host.host,
-              port: connection.session.host.port)),
+                host: connection.session.host.host,
+                port: connection.session.host.port)),
+      ),
     );
   }
 }

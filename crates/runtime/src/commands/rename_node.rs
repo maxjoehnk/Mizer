7f7c7f3ff3f7ff54bd5599172eb3a150739cfdb9
Max Jöhnk <maxjoehnk@gmail.com>
2026-08-14 16:40:48 +0200

    fix(nodes): renaming container will not update layouts referencing children

--- crates/runtime/src/commands/rename_node.rs
+++ crates/runtime/src/commands/rename_node.rs
@@ -2,7 +2,7 @@ use serde::{Deserialize, Serialize};
 
 use crate::Pipeline;
 use mizer_commander::{Command, Ref, RefMut};
-use mizer_layouts::LayoutStorage;
+use mizer_layouts::{Layout, LayoutStorage};
 use mizer_node::NodePath;
 use mizer_nodes::{ContainerNode, Node};
 
@@ -14,7 +14,7 @@ pub struct RenameNodeCommand {
 
 impl<'a> Command<'a> for RenameNodeCommand {
     type Dependencies = (RefMut<Pipeline>, Ref<LayoutStorage>);
-    type State = Vec<(NodePath, Node)>;
+    type State = (Vec<(NodePath, Node)>, Vec<(NodePath, NodePath)>);
     type Result = ();
 
     fn label(&self) -> String {
@@ -26,23 +26,32 @@ impl<'a> Command<'a> for RenameNodeCommand {
         (pipeline, layout_storage): (&mut Pipeline, &LayoutStorage),
     ) -> anyhow::Result<(Self::Result, Self::State)> {
         pipeline.rename_node(&self.path, self.new_name.clone())?;
-        self.rename_container_children(pipeline)?;
+        let mut layouts = layout_storage.read();
+        let renamed_children = self.rename_container_children(&mut layouts, pipeline)?;
         let previous_containers = self.rename_node_in_containers(pipeline)?;
-        self.rename_node_in_layouts(layout_storage, &self.path, &self.new_name);
+        self.rename_node_in_layouts(&mut layouts, &self.path, &self.new_name);
+        layout_storage.set(layouts);
 
-        Ok(((), previous_containers))
+        Ok(((), (previous_containers, renamed_children)))
     }
 
     fn revert(
         &self,
         (pipeline, layout_storage): (&mut Pipeline, &LayoutStorage),
-        container_commands: Self::State,
+        (container_commands, renamed_children): Self::State,
     ) -> anyhow::Result<()> {
         pipeline.rename_node(&self.new_name, self.path.clone())?;
+        let mut layouts = layout_storage.read();
         for (path, container) in container_commands {
             pipeline.update_node(&path, container)?;
         }
-        self.rename_node_in_layouts(layout_storage, &self.new_name, &self.path);
+        self.rename_node_in_layouts(&mut layouts, &self.new_name, &self.path);
+        self.update_container_children(pipeline, &self.path, renamed_children.iter().map(|(path, _)| path.clone()).collect());
+        for (old_path, new_path) in renamed_children {
+            self.rename_node_in_layouts(&mut layouts, &new_path, &old_path);
+            pipeline.rename_node(&new_path, old_path)?;
+        }
+        layout_storage.set(layouts);
 
         Ok(())
     }
@@ -51,12 +60,11 @@ impl<'a> Command<'a> for RenameNodeCommand {
 impl RenameNodeCommand {
     fn rename_node_in_layouts(
         &self,
-        layout_storage: &LayoutStorage,
+        layouts: &mut Vec<Layout>,
         from: &NodePath,
         to: &NodePath,
     ) {
-        let mut layouts = layout_storage.read();
-        for layout in &mut layouts {
+        for layout in layouts {
             for control in &mut layout.controls {
                 if let mizer_layouts::ControlType::Node { path } = &mut control.control_type {
                     if path == from {
@@ -65,9 +73,9 @@ impl RenameNodeCommand {
                 }
             }
         }
-        layout_storage.set(layouts);
     }
 
+    /// Update the reference to the renamed node in the parent container
     fn rename_node_in_containers(
         &self,
         pipeline: &mut Pipeline,
@@ -102,8 +110,10 @@ impl RenameNodeCommand {
             .collect()
     }
 
+    /// Renames all children of a container to now have the updated prefix
     fn rename_container_children(
         &self,
+        layouts: &mut Vec<Layout>,
         pipeline: &mut Pipeline,
     ) -> anyhow::Result<Vec<(NodePath, NodePath)>> {
         let Some(container) = pipeline.get_node::<ContainerNode>(&self.new_name) else {
@@ -115,13 +125,18 @@ impl RenameNodeCommand {
             if path.starts_with(&self.path) {
                 let new_path = path.replace_prefix(&self.path, &self.new_name);
                 pipeline.rename_node(&path, new_path.clone())?;
+                self.rename_node_in_layouts(layouts, &path, &new_path);
                 renamed_paths.push((path, new_path));
             }
         }
-        if let Some(container) = pipeline.get_node_mut::<ContainerNode>(&self.new_name) {
-            container.nodes = renamed_paths.iter().map(|(_, path)| path.clone()).collect();
-        }
+        self.update_container_children(pipeline, &self.new_name, renamed_paths.iter().map(|(_, path)| path.clone()).collect());
 
         Ok(renamed_paths)
+    }
+
+    fn update_container_children(&self, pipeline: &mut Pipeline, container: &NodePath, children: Vec<NodePath>) {
+        if let Some(container) = pipeline.get_node_mut::<ContainerNode>(&container) {
+            container.nodes = children;
+        }
     }
 }
